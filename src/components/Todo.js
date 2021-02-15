2d89c8eadec939828e837920c94db0062b886dc4
ka123-web <cavi.vpm@gmail.com>
2021-02-15 17:43:11 -0500

    todo

---
+++ src/components/Todo.js
@@ -0,0 +1,48 @@
+import React,{useState} from 'react';
+import TodoList from './TodoList';
+import {RiCloseCircleLine} from 'react-icons/ri';
+import {TiEdit} from 'react-icons/ti';
+import TodoForm from './TodoForm';
+
+const Todo = ({todos,completeTodo,removeTodo,updateTodo}) =>{
+    const[editItem,setEditItem] = useState({
+        id:null,
+        value:''
+    })
+
+    const submitUpdate =(value) =>{
+        updateTodo (editItem.id,value)
+        setEditItem({
+            id:null,
+            value:""
+        })
+    }
+
+    if(editItem.id){
+        return <TodoForm edit={editItem} onSubmit ={submitUpdate}/>
+    }
+    return (
+    
+       <div>
+            {todos.map((todo,index) =>(
+                <div className={todo.isComplete? 'todo-row complete': 'todo-row' } 
+                    key ={index}>
+
+                        <div
+                        key={todo.id} onClick = {()=> completeTodo(todo.id)}>
+                           {todo.text}
+                        </div>
+                        <div className="icons">
+                            <RiCloseCircleLine onClick={()=>removeTodo(todo.id)}
+                            className="delete-icon"/>
+
+                            <TiEdit onClick={()=>setEditItem({id:todo.id, value:todo.text})}
+                            className="edit-icon"/>
+                        </div>
+                </div>
+            ))}
+      </div> 
+    )
+}
+
+export default Todo
---
+++ src/components/TodoForm.js
@@ -0,0 +1,63 @@
+import React,{useState,useEffect,useRef} from 'react'
+
+const TodoForm =(props) =>{
+   const [input, setInput]=useState(props.edit?props.edit.value:'');
+
+   const inputRef = useRef(null);
+
+   useEffect(()=>
+   {
+       inputRef.current.focus();
+   })
+   const submitHandler =(e) =>
+   {
+       e.preventDefault();
+       props.onSubmit(
+        {
+            id: Math.floor(Math.random()*10000),
+            text :input,
+        });
+        setInput('');
+   }
+   const inputChangeHandler = (e) =>
+   {
+    setInput(e.target.value);
+   }
+   
+  
+    return (
+           <form onSubmit={submitHandler} className="todo-form">
+               {props.edit?(
+            <>
+               <input
+               className="todo-input edit" 
+               type="text"
+               value={input}
+               placeholder="Update your item"
+               onChange={inputChangeHandler}
+               ref ={inputRef}
+               >
+               </input>
+               <button className="todo-button edit" > Update</button> </>
+            ):(
+                <> 
+              <input
+               className="todo-input" 
+               type="text"
+               value={input}
+               placeholder="add a to do"
+               onChange={inputChangeHandler}
+               ref ={inputRef}
+               >
+               </input>
+               <button className="todo-button" > Add todo</button> </>
+               )
+              
+               }
+               
+            </form> 
+        
+    );
+}
+
+export default TodoForm
